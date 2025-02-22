d604a7383470da2ddbeb13ca024e503959f7d3e7
Williamf <williamf.heraim.1@educa.jcyl.es>
2025-02-22 04:50:44 +0100

    William: He creado una calculadora muy basica, sin validacion ni nada

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,41 @@
 function holaMundo(){
     /*Abre una ventana emergente*/
+
     alert ("Hola Mundo, primer programa en java. Jeje")
 }
+
+/*
+Modificación de William
+*/
+
+function sumar() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let sum = Number(num1) + Number(num2);
+    document.getElementById('Resultado').innerHTML = "La suma es: " + sum;
+}
+
+function restar() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let resta = Number(num1) - Number(num2);
+    document.getElementById('Resultado').innerHTML = "La resta es: " + resta;
+}
+
+function multiplicar() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let multiplicacion = Number(num1) * Number(num2);
+    document.getElementById('Resultado').innerHTML = "La multiplicación es: " + multiplicacion;
+}
+
+function dividir() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let division = Number(num1) / Number(num2);
+    document.getElementById('Resultado').innerHTML = "La división es: " + division;
+}
+
+
+
+ 
